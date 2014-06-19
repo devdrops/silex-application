fe9deaaa436dec448cc81803d9c13cc34cf559aa
Davi Marcondes Moreira <davi.marcondes.moreira@gmail.com>
2014-06-18 22:48:55 -0300

    Error handler and conditional messages. Also documentation for actions, on app.php.

--- app/app.php
+++ app/app.php
@@ -6,12 +6,25 @@ use Symfony\Component\HttpFoundation\Response;
 
 $app = new Silex\Application();
 
+/*
+ * Home action.
+ */
 $app->get('/', function(){
     return new Response('Welcome to my new Silex App!');
 });
 
+/*
+ * Error handler
+ */
 $app->error(function(\Exception $exc, $code){
-    return new Response('Oops! Something went terribly wrong...: ' . $exc->getMessage());
+    switch ($code){
+        case 404:
+            $message = 'Oops! Something went terribly wrong...';
+            break;
+        default:
+            $message = 'Oops! Something went wrong, but no worries :)';
+    }
+    return new Response($message . ': ' . $exc->getMessage());
 });
 
 return $app;
